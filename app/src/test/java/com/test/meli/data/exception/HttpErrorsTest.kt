9b376084144cc3a4a088a20c9b773a1bc29b61e8
rovargas <uGyh6P6T.J6_j8N>
2022-05-27 06:06:43 -0500

    test

--- app/src/test/java/com/test/meli/data/exception/HttpErrorsTest.kt
+++ app/src/test/java/com/test/meli/data/exception/HttpErrorsTest.kt
@@ -1,10 +1,15 @@
 package com.test.meli.data.exception
 
 import org.junit.Before
+import org.junit.Test
 
 internal class HttpErrorsTest {
 
     @Before
     fun setUp() {
+    }
+
+    @Test
+    fun prueba() {
     }
 }
