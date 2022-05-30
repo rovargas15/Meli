f4fce12a3d6ec402934e3ed4805f9b33c5c64203
rovargas <uGyh6P6T.J6_j8N>
2022-05-29 19:49:22 -0500

    add unit test CommonErrorsTest.kt

--- app/src/test/java/com/test/meli/domain/exception/CommonErrorsTest.kt
+++ app/src/test/java/com/test/meli/domain/exception/CommonErrorsTest.kt
@@ -1,10 +1,60 @@
 package com.test.meli.domain.exception
 
-import org.junit.Before
+import com.squareup.moshi.JsonDataException
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.net.ConnectException
+import java.net.SocketTimeoutException
 
 class CommonErrorsTest {
 
-    @Before
-    fun setUp() {
+    private val commonErrors = CommonErrors()
+
+    @Test
+    fun giveThrowableWhenManageJavaErrorsThenReturnTimeOutException() {
+        // Give
+        val exception = SocketTimeoutException()
+
+        // When
+        val domainException = commonErrors.manageException(exception)
+
+        // Then
+        assertEquals(domainException, TimeOutException)
+    }
+
+    @Test
+    fun giveThrowableWhenManageJavaErrorsThenReturnInternalErrorException() {
+        // Give
+        val exception = ConnectException()
+
+        // When
+        val domainException = commonErrors.manageException(exception)
+
+        // Then
+        assertEquals(domainException, InternalErrorException)
+    }
+
+    @Test
+    fun giveThrowableWhenManageJavaErrorsThenReturnJsonDataException() {
+        // Give
+        val exception = JsonDataException()
+
+        // When
+        val domainException = commonErrors.manageException(exception)
+
+        // Then
+        assertEquals(domainException, ParseException)
+    }
+
+    @Test
+    fun giveThrowableWhenManageJavaErrorsThenReturnUnknownError() {
+        // Give
+        val exception = IllegalArgumentException()
+
+        // When
+        val domainException = commonErrors.manageException(exception)
+
+        // Then
+        assertEquals(domainException, UnknownError)
     }
 }
